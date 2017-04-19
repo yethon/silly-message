4aed44c345899c46c7fcbcc670640121334f2240
yethon <ursula_yethon@hotmail.com>
2017-04-19 09:56:03 -0400

    Display hysterical Ron Swanson quotes.

--- src/ui/components/silly-message/component.ts
+++ src/ui/components/silly-message/component.ts
@@ -1,5 +1,21 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from '@glimmer/component';
 
-export default class SillyMessage extends Component {
+class testname extends Component {
+  @tracked
+  message: any;
 
+  constructor(options) {
+    super(options);
+    this.loadMessage();
+    setInterval(() => { this.loadMessage(); }, 3500);
+  }
+
+  async loadMessage() {
+    let request = await fetch('http://ron-swanson-quotes.herokuapp.com/v2/quotes');
+    let quote = await request.json();
+
+    this.message=quote[0];
+  }
 }
+
+export default testname;
